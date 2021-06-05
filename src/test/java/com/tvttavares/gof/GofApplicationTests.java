cb2cd4dd4bb4051a8619311b9dffe9ef358c7068
Thiago Tavares <tvttavares@gmail.com>
2021-06-05 19:19:33 -0400

    Initial Commit

---
+++ src/test/java/com/tvttavares/gof/GofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvttavares.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
